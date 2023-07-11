4780b95a5638f3024f1fc9098e935b97a42ada40
chimes512 <7fw@ro.ru>
2023-07-11 16:18:55 -0400

    Update visuals.js
    IMAGINE IF

--- visuals.js
+++ visuals.js
@@ -1,31 +1,37 @@
 export function visuals() {
-    //CHINODUBY THEM SONG1!!
-    var CHINODUBYsONG = new Audio("https://cdn.discordapp.com/attachments/1105181766320394312/1128138397056245790/ytmp3free.cc_dr-i-keep-holding-on-my-hope-will-never-die-youtubemp3free.org.mp3");
-    CHINODUBYsONG.addEventListener('ended', function() {
-        this.currentTime = 0;
-        this.play();
-    }, false);
-    CHINODUBYsONG.addEventListener('', function() {
-        CHINODUBYsONG.play();
-    });
-    //CHINODUBY KREDITZZ!!1
-    alert("Gracias Por Usar..::SpeedLight V3®️::..️ ..::By cHiNoDuBy™️::..️"); 
-    alert("SO Modificado Por ..::cHiNoDuBy™️::..®️"); 
-    alert("Para mas informacion:Speedlightv2.blogspot.com ..::By cHiNoDuBy™️::.."); 
-    alert("contactar En:chinodubyduby @hotmail.com ..::By cHiNoDuBy™️::..®️"); 
-    alert("..::SpeedLight Edition®️::.."); 
-    alert(".-'¯'-.><((((°> ..::By cHiNoDuBy™️::.. <°))))><.-'¯'-."); 
-    alert("WELCOME TO WINDOWS®️ SpeedLight®️ V 3®️ by ..::cHiNoDuBy::..®️"); 
-    alert("by ..::cHiNoDuBy™️::..®️");
+//CHINODUBY THEM SONG1!!
+var CHINODUBYsONG = new Audio("https://cdn.discordapp.com/attachments/1105181766320394312/1128415242976763947/ytmp3free.cc_dr-i-keep-holding-on-my-hope-will-never-die-youtubsdvemp3free.org_11.mp3");
+CHINODUBYsONG.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
+document.body.addEventListener('mousemove', function() {
+    CHINODUBYsONG.play();
+});
+document.body.addEventListener('touchstart', function() {
+    CHINODUBYsONG.play();
+});
+//CHINODUBY KREDITZZ!!1
+alert("Gracias Por Usar..::SpeedLight V3®️::..️ ..::By cHiNoDuBy™️::..️"); 
+alert("SO Modificado Por ..::cHiNoDuBy™️::..®️"); 
+alert("Para mas informacion:Speedlightv2.blogspot.com ..::By cHiNoDuBy™️::.."); 
+alert("contactar En:chinodubyduby @hotmail.com ..::By cHiNoDuBy™️::..®️"); 
+alert("..::SpeedLight Edition®️::.."); 
+alert(".-'¯'-.><((((°> ..::By cHiNoDuBy™️::.. <°))))><.-'¯'-."); 
+alert("WELCOME TO WINDOWS®️ SpeedLight®️ V 3®️ by ..::cHiNoDuBy::..®️"); 
+alert("by ..::cHiNoDuBy™️::..®️");
 
-    //IMIGES!
-    document.body.style.backgroundImage = "url('https://i.imgur.com/m9ZfUvI.png')";
-    var MANEeLEMENT = document.getElementsByClassName("fs-main");
-    var SUEBHADeLEMENT = document.getElementsByClassName("fs-subhead__wrapper");
-    for (let i = 0; i < MANEeLEMENT.length; i++) {
-        MANEeLEMENT[i].style.backgroundImage = "url('https://i.imgur.com/bHFqYo3.png')"
-    };
-    for (let i = 0; i < SUEBHADeLEMENT.length; i++) {
-        SUEBHADeLEMENT[i].style.backgroundImage = "url('https://i.imgur.com/FQ4P07L.png')"
-    };
+//IMIGES!
+document.body.style.backgroundImage = "url('https://i.imgur.com/m9ZfUvI.png')";
+document.getElementsByClassName("fs-logo__image")[0].src = "//fontstruct.com/avatar/2310433/chinodubytm.png?version=66a732380f94c612fbca17ffbb544341";
+var MANEeLEMENT = document.getElementsByClassName("fs-main");
+var SUEBHADeLEMENT = document.getElementsByClassName("fs-subhead__wrapper");
+for (let i = 0; i < MANEeLEMENT.length; i++) {
+    MANEeLEMENT[i].style.backgroundImage = "url('https://i.imgur.com/bHFqYo3.png')"
+};
+for (let i = 0; i < SUEBHADeLEMENT.length; i++) {
+    SUEBHADeLEMENT[i].style.backgroundImage = "url('https://i.imgur.com/FQ4P07L.png')"
+};
+
+
 }
